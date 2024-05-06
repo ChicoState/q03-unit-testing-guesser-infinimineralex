818c1e1003e6d48a4bfa8345a38f5c552d892e64
Alexander Haga <ivoRobotnik36@gmail.com>
2024-05-05 19:48:59 -0700

    Update GuesserTest.cpp

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -7,6 +7,14 @@
 #include "Guesser.h"
 
 using std::string;
+class GuesserTest : public ::testing::Test
+{
+	protected:
+		GuesserTest(){} //constructor runs before each test
+		virtual ~GuesserTest(){} //destructor cleans up after tests
+		virtual void SetUp(){} //sets up before each test (after constructor)
+		virtual void TearDown(){} //clean up after each test, (before destructor)
+};
 
 TEST(GuesserTest, MatchCorrectGuess) {
     Guesser g("abcdefghijklmnopqrstuvwxyz");
